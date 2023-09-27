e37012982d55ea90bac83affd83e48e11ff3a528
Arthur Soares <arthur.soares@ifood.com.br>
2023-09-26 22:43:35 -0300

    keep server on

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("Testing...");
+const delay = ms => new Promise(res => setTimeout(res, ms));
+
+
+(async () => {
+    while (1) {
+        console.log("Testing...");
+        await delay(1000);
+    }
+})()
